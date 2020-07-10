ff741741653fd016b930cb0559203af726a316b0
Joshua Lin <42842303+jlin-orange@users.noreply.github.com>
2020-07-09 21:22:28 -0500

    changed variable names for bounds

--- spiralMatrix1.java
+++ spiralMatrix1.java
@@ -28,17 +28,17 @@ class Solution {
     */
     public List<Integer> spiralOrder(int[][] matrix) {       
         // list for returning elements in spiral order
-        List<Integer> l = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         
-        int height = matrix.length; if (height < 1) {return l;}
+        int height = matrix.length; if (height < 1) return result;
         int width = matrix[0].length;
         int n = height*width;
         
         // Initial Left/Right/Top/Bottom bounds respectively
-        int lb = -1;
-        int rb = width;
-        int tb = 0;
-        int bb = height; // # of columns
+        int leftBound = -1;
+        int rightBound = width;
+        int topBound = 0;
+        int bottomBound = height; // # of columns
         
         // x,y = col index,row index
         // matrix[y][x] = matrix[row][col]
@@ -49,13 +49,13 @@ class Solution {
         int dir = 0;
         
         for (int i = 1; i <= n; i++) {
-            l.add(matrix[y][x]);
+            result.add(matrix[y][x]);
             switch (dir) {
                 // Check bounds @ each element: 
                     // if out-of-bound, change direction/update bounds
                 case 0: // turn down [for what!]
-                    if (x+1 == rb) { 
-                        y++; rb--;
+                    if (x+1 == rightBound) { 
+                        y++; rightBound--;
                         dir = 1;
                     } else {
                         x++;
@@ -63,8 +63,8 @@ class Solution {
                     break;
                 
                 case 1: // Down
-                    if (y+1 == bb ) { // turn left
-                        x--; bb--;
+                    if (y+1 == bottomBound ) { // turn left
+                        x--; bottomBound--;
                         dir = 2;
                     } else {
                         y++;
@@ -72,8 +72,8 @@ class Solution {
                     break;
                     
                 case 2: // Left
-                    if (x-1 == lb) { // turn up
-                        y--; lb++;
+                    if (x-1 == leftBound) { // turn up
+                        y--; leftBound++;
                         dir = 3;
                     } else {
                         x--;
@@ -81,8 +81,8 @@ class Solution {
                     break;
                 
                 case 3: // Up
-                    if (y-1 == tb) { // turn right
-                        x++; tb++;
+                    if (y-1 == topBound) { // turn right
+                        x++; topBound++;
                         dir = 0;
                     } else {
                         y--;
@@ -90,6 +90,6 @@ class Solution {
                     break;
             }
         }
-        return l;
+        return result;
     }
 }
