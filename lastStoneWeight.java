cb1c99960b9785e96d89066de2c7a292db42f6d2
Joshua Lin <42842303+jlin-orange@users.noreply.github.com>
2020-07-11 22:24:31 -0500

    Update lastStoneWeight.java

--- lastStoneWeight.java
+++ lastStoneWeight.java
@@ -1,15 +1,18 @@
 class Solution {
     /*
-    Make a max heap of the stones before greedily smashing the 
-    2 heaviest stones + updating the heap afterwards until there
-    is >=1 stone left.
+    Gist: Make a max heap of the stones before greedily smashing the 
+    2 heaviest stones. Always update the heap afterwards until there
+    is x<=1 stone left.
     
-    Use a priority queue to implement a max heap.
-    Loop to add all elemenets of stones to heap.
-    Take 2 largest stones, "smash them", then re-insert remainder stone (if there is one)
+    Steps
+    1.Use a priority queue to implement a max heap.
+    2. Loop to add all elements of stones to heap.
+    3. While heap has >1 stones, 
+        take 2 largest stones, "smash them", then re-insert remainder stone (if there is one)
+    4. Return final stone (if exists)
     
     Time: O(nlgn) - heapifying (both upon intial insertion/re-insertion of remainder stones)
-    Space: O(n) - heap must be contain all stones
+    Space: O(n) - heap must contain all stones
     */
     public int lastStoneWeight(int[] stones) {
         PriorityQueue<Integer> stoneMaxHeap = new PriorityQueue<>(Collections.reverseOrder());
@@ -29,7 +32,7 @@ class Solution {
             } 
         }
 
-        int lastStone = stoneMaxHeap.peek() == null ? 0 : stoneMaxHeap.poll();
+        int lastStone = stoneMaxHeap.isEmpty() ? 0 : stoneMaxHeap.poll();
         return lastStone;
     }
 }
