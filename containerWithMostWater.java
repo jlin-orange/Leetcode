7cdbd9cd5aa40ff61aa8aa492edd847337e7dcaa
Joshua Lin <42842303+jlin-orange@users.noreply.github.com>
2020-07-21 21:20:17 -0500

    Update containerWithMostWater.java

--- containerWithMostWater.java
+++ containerWithMostWater.java
@@ -5,22 +5,23 @@ class Solution {
     Time: O(n), Space: O(1)
     */
     public int maxArea(int[] height) {
-        int low = 0; 
-        int high = height.length - 1;
+        int left = 0; 
+        int right = height.length - 1;
         int length;
         
         int maxArea = 0; int currMax = 0;
         
         // Two pointers iterate towards one another while keeping track of max area
-        while (low < high) {
-            length = high - low;
-            currMax = length * Math.min(height[low], height[high]);
+        while (left < right) {
+            length = right - left;
+            currMax = length * Math.min(height[left], height[right]);
             maxArea = Math.max(currMax, maxArea);
-                
-            if (height[low] < height[high]) {
-                low++;
+            
+            // Look for next tallest line
+            if (height[left] < height[right]) {
+                left++;
             } else {
-                high--;
+                right--;
             }
         }
         
