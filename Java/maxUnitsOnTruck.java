64e651a5d9348f90efd005a31371092d90390928
Josh Lin <42842303+jlin-orange@users.noreply.github.com>
2021-01-14 23:59:27 -0600

    finished greedy solution

--- Java/maxUnitsOnTruck.java
+++ Java/maxUnitsOnTruck.java
@@ -1,24 +1,30 @@
-package Java;
-
-public class maxUnitsOnTruck {
+class Solution {
+    // Runtime: O(truckSize), Space: O(1)
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        int totalUnits = 0;
-        int currCapacity = truckSize;
-           
+        // 1.) sort the boxTypes by # of units! 
+        // 2.) take as many boxes as you can, starting with the boxes containing the highest amount of units
+        
+        int remainingSpace = truckSize;
+        int maxUnits = 0;
+        
+        Arrays.sort(boxTypes, (a, b) -> Double.compare(a[1], b[1]));
+        
         int totalTypes = boxTypes.length;
+        if (totalTypes == 0) return maxUnits;
+        
+        // Greedily add high-unit boxes (which we sorted for) till they're out of stock
         for (int i = totalTypes - 1; i >= 0; i--) {
-            int numOfBoxes = boxTypes[i][0];
-            int unitsPerBox = boxTypes[i][1];
-            
-            for (i = 0; i < numOfBoxes; i++) {
-                if (currCapacity - unitsPerBox >= 0) {
-                    numOfBoxes--; totalUnits++;
-                    currCapacity -= unitsPerBox;
-                } else {
-                    break;
-                }
+            while (boxTypes[i][0] > 0) {
+                maxUnits += boxTypes[i][1]; 
+                
+                // Update the array + truck space
+                boxTypes[i][0]--;
+                remainingSpace--;
+                
+                if (remainingSpace == 0) return maxUnits;
             }   
         }
-        return totalUnits;
+    
+        return maxUnits;
     }
 }
